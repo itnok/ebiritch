90506b59a8b1a25fa66a5fbfb6bfe11fa083d9c1
Simone Conti <s.conti@itnok.com>
2020-12-28 23:07:07 -0800

    [SETUP] Basic Python project scaffolding

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# release markers:
+#   X.Y
+#   X.Y.Z   # For bugfix releases
+#
+# pre-release markers:
+#   X.YaN   # Alpha release
+#   X.YbN   # Beta release
+#   X.YrcN  # Release Candidate
+#   X.Y     # Final release
+
+VERSION = "0.1.0"
